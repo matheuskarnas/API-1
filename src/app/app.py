8233247f6b5d8d26e36516a38493928a8bf525b2
PedHr <pedrohe313131@gmail.com>
2024-09-29 10:00:39 -0300

    UPDATE: rote update

--- src/app/app.py
+++ src/app/app.py
@@ -5,22 +5,21 @@ app = Flask(
   __name__, template_folder="front/templates", static_folder="front/static"
   )
 
-# Criação de Rotas
+# ROTA PARA PÁGINA INDEX
 @app.route("/")
 def home():
   return render_template('index.html')
 
-@app.route("/information.html")
+
+# ROTA PARA PÁGINA DE INFORMAÇÃO
+@app.route("/informationPage.html")
 def information():
-  return render_template('information.html')
+  return render_template('informationPage.html')
 
-@app.route("/comp_vereador.html")
+# ROTA PARA PÁGINA DE COMPARAÇÃO
+@app.route("/comparationPage.html")
 def comp_vereador():
-  return render_template('comp_vereador.html')
-
-@app.route("/teste.html")
-def teste():
-  return render_template('teste.html')
+  return render_template('comparationPage.html')
 
 app.run(debug=True)
 
