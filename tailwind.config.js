4866324590f349b05dcb2e64654f587ed28520d8
ThOMaZMe11o <thomazmello1912@gmail.com>
2024-10-09 07:47:56 -0300

    tailwind config cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': '#fff',
+        'black': '#000',
+        'gray': '#6b7280',
+        'red': '#ef4444',
+        'blue': '#1d74e6'      
+      }
+    },
   },
   plugins: [],
 }
